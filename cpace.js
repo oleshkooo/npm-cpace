47d6af0418b525fe482e5b0a379ded00bac1af13
Oleshkooo <olehchoma@gmail.com>
2022-10-05 17:12:21 +0300

    formatted

--- cpace.js
+++ cpace.js
@@ -1,38 +1,33 @@
 #!/usr/bin/env node
 
-
 import fs from 'fs'
+
 import { spawn, execFile } from 'child_process'
+
 import chalk from 'chalk'
 
-
 const args = process.argv.slice(2)
-const packageName = "cpace"
+const packageName = 'cpace'
 const prefix = `  [${packageName}]`
 const blue = chalk.rgb(101, 155, 211)
 const red = chalk.rgb(255, 0, 0).bold
-
 
 let helpF = false
 let compileOnlyF = false
 let directoryF = false
 let directoryFIndex
 args.map((item, index) => {
-    if (item === '--help' || item === '-h')
-        helpF = true
-    if (item === '--compile' || item === '-c')
-        compileOnlyF = true
+    if (item === '--help' || item === '-h') helpF = true
+    if (item === '--compile' || item === '-c') compileOnlyF = true
     if (item === '--directory' || item === '-d') {
         directoryF = true
         directoryFIndex = index
     }
 })
 
-
 const app = {
-    init: function() {
-        if (helpF)
-            this.help()
+    init: function () {
+        if (helpF) this.help()
 
         console.log(blue(`${prefix} Starting ${packageName}`))
 
@@ -44,7 +39,7 @@ const app = {
 
         console.log(blue(`${prefix} Watching extensions: .c, .cpp`))
 
-        let file = {
+        const file = {
             directory: '',
             name: '',
             extension: '',
@@ -62,8 +57,7 @@ const app = {
             file.watchPath = file.directory
             file.compilePath = file.directory + '/' + file.name
             console.log(file)
-        }
-        else {
+        } else {
             file.name = args[0]
 
             this.getExtension(file)
@@ -76,9 +70,7 @@ const app = {
         this.watchFile(file)
     },
 
-
-
-    watchFile: function(file) {
+    watchFile: function (file) {
         let fsWait = false
 
         console.log(blue(`${prefix} Watching '${file.watchPath}'`))
@@ -95,24 +87,33 @@ const app = {
             }
         })
     },
-    compileFile: function(file) {
+    compileFile: function (file) {
         let done = false
         let err = false
         let compileFile
 
-        if (file.extension === '.cpp')
-            compileFile = spawn('g++', ['-o', file.name + file.exe, file.compilePath + file.extension])
-        else
-            compileFile = spawn('gcc', ['-o', file.name + file.exe, file.compilePath + file.extension])
+        if (file.extension === '.cpp') {
+            compileFile = spawn('g++', [
+                '-o',
+                file.name + file.exe,
+                file.compilePath + file.extension,
+            ])
+        } else {
+            compileFile = spawn('gcc', [
+                '-o',
+                file.name + file.exe,
+                file.compilePath + file.extension,
+            ])
+        }
 
-        compileFile.on('error', (error) => {
+        compileFile.on('error', error => {
             if (!done) {
                 console.error(blue(prefix) + red(' Error\n') + error)
                 done = true
             }
             err = true
         })
-        compileFile.stderr.on('data', (data) => {
+        compileFile.stderr.on('data', data => {
             if (!done) {
                 console.error(blue(prefix) + red(' Compiling error'))
                 console.error(this.decode(data))
@@ -120,38 +121,43 @@ const app = {
             }
             err = true
         })
-        compileFile.stdout.on('data', (data) => {
+        compileFile.stdout.on('data', data => {
             console.log(data)
         })
         compileFile.on('exit', (code, signal) => {
             if (!err) {
                 console.log(blue(`${prefix} Compiled successfully`))
 
-                if (!compileOnlyF)
-                    this.openFile(file)
+                if (!compileOnlyF) this.openFile(file)
             }
         })
     },
-    openFile: function(file) {
+    openFile: function (file) {
         execFile(file.name + file.exe, (error, stdout, stderr) => {
-            if (error)
-                return console.error(blue(prefix) + red(' .exe Error\n') + error)
-            if (stderr)
-                return console.error(blue(prefix) + red(' .exe StdError\n') + error)
+            if (error) return console.error(blue(prefix) + red(' .exe Error\n') + error)
+            if (stderr) return console.error(blue(prefix) + red(' .exe StdError\n') + error)
 
             console.log(stdout)
         })
     },
 
+    help: function () {
+        const tab = '  '
 
-    help: function() {
-        let tab = '  '
-        console.log(blue(`\n${tab}${packageName} is used to automatically compile and run\n\ta [.c] or [.cpp] file when it is modified`))
+        console.log(
+            blue(
+                `\n${tab}${packageName} is used to automatically compile and run\n\ta [.c] or [.cpp] file when it is modified`,
+            ),
+        )
 
         console.log(blue(`\n\n${tab}Options:`))
         console.log(blue(`${tab}    -h, --help ............ open CLI options`))
-        console.log(blue(`${tab}    -c, --compile ......... compilation without starting the program`))
-        console.log(blue(`${tab}    -d, --directory ....... watch a directory and compile specific file`))
+        console.log(
+            blue(`${tab}    -c, --compile ......... compilation without starting the program`),
+        )
+        console.log(
+            blue(`${tab}    -d, --directory ....... watch a directory and compile specific file`),
+        )
         console.log(blue(`${tab}\t\t\t      (the file name must be after the -d argument)`))
 
         console.log(blue(`\n\n${tab}Examples:`))
@@ -164,28 +170,25 @@ const app = {
         process.exit()
     },
 
-    getExtension: function(file) {
+    getExtension: function (file) {
         if (file.name.includes('.cpp')) {
             file.extension = '.cpp'
             file.name = file.name.substring(0, file.name.length - 4)
-        }
-        else if (file.name.includes('.c')) {
+        } else if (file.name.includes('.c')) {
             file.extension = '.c'
             file.name = file.name.substring(0, file.name.length - 2)
-        }
-        else {
+        } else {
             file.extension = ''
             console.error(blue(prefix) + red(' Error: ') + blue('Wrong flag or file extension'))
             process.exit()
         }
     },
 
-    decode: function(data) {
-        let decoded = data.toString('utf8')
+    decode: function (data) {
+        const decoded = data.toString('utf8')
         return decoded
-    }
+    },
 }
-
 
 app.init()
 export { app }
